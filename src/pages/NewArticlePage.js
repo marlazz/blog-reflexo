f046f3e94a1451752aee5d14ac5dae393a5b9aa6
Marla <zingermarla8@gmail.com>
2022-09-03 19:42:32 +0200

    add Quill Edtior and send data to firestore. The data is stored as html in a string

--- src/pages/NewArticlePage.js
+++ src/pages/NewArticlePage.js
@@ -1,32 +1,54 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { addDoc, collection } from "firebase/firestore";
-import { db, auth } from "../firebase-config";
+import { db } from "../firebase-config";
+import "react-quill/dist/quill.snow.css";
+import ReactQuill from "react-quill";
 
 const NewArticlePage = ({ isAuth }) => {
   const [title, setTitle] = useState("");
   const [postContent, setPostContent] = useState("");
+  const [articleContent, setArticleContent] = useState("");
 
   const postsCollectionRef = collection(db, "posts");
+
   let navigate = useNavigate();
 
+  const addArticleContent = (value) => {
+    setArticleContent(value)
+  }
   const createPost = async () => {
     await addDoc(postsCollectionRef, {
       title,
-      postContent,
-      // author: { name: auth.currentUser, id: auth.currentUser},
+      articleContent,
     });
-    alert('Nouvel article ajouté');
-    setTitle("")
-    setPostContent("")
-    // to do: clear form inputs once its submited
+    alert("Nouvel article ajouté");
+    setTitle("");
+    setPostContent("");
   };
 
   useEffect(() => {
     if (isAuth) {
       navigate("/login");
     }
-  },);
+  });
+
+  console.log("postContent", postContent)
+
+  const modules = {
+    toolbar: [
+      [{ header: [2, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image"],
+      ["clean"],
+    ],
+  };
 
   return (
     <div>
@@ -44,7 +66,7 @@ const NewArticlePage = ({ isAuth }) => {
             }}
           />
         </div>
-        <div>
+        {/* <div>
           <label htmlFor="content">Article:</label>
           <textarea
             id="content"
@@ -53,6 +75,13 @@ const NewArticlePage = ({ isAuth }) => {
             onChange={(event) => {
               setPostContent(event.target.value);
             }}
+          />
+        </div> */}
+        <div>
+          <ReactQuill
+            theme="snow"
+            modules={modules}
+            onChange={addArticleContent}
           />
         </div>
         <button onClick={createPost}> Submit Post</button>
