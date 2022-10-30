55366b760c0164e8df73c67889bc122ab6e26d78
Marla <zingermarla8@gmail.com>
2022-10-30 20:03:45 +0100

    desktop intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,21 +1,22 @@
+import PORTRAIT from "../assets/leila.png";
 import "./Intro.css";
 
 const Intro = () => {
   return (
-    <div className="intro-root">
-    <h2 className="section-title">Qui suis-je ?</h2>
+    <div className="intro">
+      <img src={PORTRAIT} alt="portrait de Leila Elghanan" className="intro-portrait" />
       <div className="text-container">
+        <h2 className="intro-title">Bienvenue! je suis Leila El Ghanane</h2>
+        <hr className="decoration"/>
         <p>
           Réflexothérapeute certifiée depuis 2017 installée à Carcassonne , je
           vous propose des séances de réflexologie combinée / réflexologie
-          plantaire. Héritée de la médecine traditionnelle chinoise, la
-          réflexologie s’adresse à un large public (enfants, seniors...) et
-          permet de soulager dans de nombreux cas.
+          plantaire.
         </p>
-        <p>
+        <span>
           Vous souffrez de douleurs, de troubles liés au stress et vous
           souhaitez retrouver détente physique et psychique?
-        </p>
+        </span>
         <p>
           Je vous accompagne dans une démarche de bien-être au mieux-être à
           travers un coaching personnalisé.
@@ -23,9 +24,17 @@ const Intro = () => {
         <p>
           Vous trouverez ici plus d’informations sur mes differentes pratiques,
           n’hésitez pas à me contacter pour toutes questions. Et en
-          attendant....Prennez soin de vous!
+          attendant....
         </p>
-
+        <span>Prennez soin de vous!</span>
+        <ul className="intro-list">
+          <li>Réflexothérapie/Réflexologie Combinée</li>
+          <li>Massage et pose Ventouses</li>
+          <li>Tensions musculaires, lombalgies, fatigue passagère, stress, drainage lymphatique, sevrage tabagique...</li>
+          <li>Ecoute et accompagnement</li>
+          <li>méthode combinée Sonia Fischmann</li>
+          <li>Certification professionnelle « ventouses Shen-Ti »</li>
+        </ul>
       </div>
     </div>
   );
