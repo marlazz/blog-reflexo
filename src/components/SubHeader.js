20e5d1a2329887b0a7bc3c44f48cfc1b1c127551
Marla <zingermarla8@gmail.com>
2022-10-30 15:23:36 +0100

    responsive hero + subheader desktop

--- src/components/SubHeader.js
+++ src/components/SubHeader.js
@@ -1,60 +1,26 @@
 import "./SubHeader.css";
-import RDV from "../assets/subheader/rdv.svg";
 import { Link } from "react-router-dom";
+import data from "../data/subheader.texts";
+
 
 const SubHeader = () => {
   return (
     <section className="subHeader">
       <div className="subHeader-container">
-        <div className="subHeader-container-col">
+      { data.map((col, id) => (
+        <div className="subHeader-container-col" key={id}>
           <div className="subHeader-container-heading">
-            <img src={RDV} alt="rendez-vous" />
-            <h3 className="subHeader-title">Rendez-vous</h3>
+            <img src={col.icon} alt="rendez-vous" />
+            <h3 className="subHeader-title">{col.title}</h3>
           </div>
           <div className="subHeader-container-content">
             <p>
-              Vous pouvez prendre rendez-vous par téléphone ou email. 
+              {col.content} 
             </p>
-            <Link className="subHeader-link" to="">Infos contact</Link>
+            <Link className="subHeader-link" to="">{col.link}</Link>
           </div>
         </div>
-        <div className="subHeader-container-col">
-          <div className="subHeader-container-heading">
-            <img src={RDV} alt="rendez-vous" />
-            <h3 className="subHeader-title">Adresse</h3>
-          </div>
-          <div className="subHeader-container-content">
-            <p>Vous pouvez prendre rendez-vous par téléphone ou email.</p>
-            <Link className="subHeader-link" to="">Voir sur la carte</Link>
-          </div>
-        </div>
-        <div className="subHeader-container-col">
-          <div className="subHeader-container-heading">
-            <img src={RDV} alt="rendez-vous" />
-            <h3 className="subHeader-title">Infos</h3>
-          </div>
-          <div className="subHeader-container-content">
-            <p>
-              Vous pouvez prendre rendez-vous 
-              par téléphone Vous 
-              pouvez prendre
-              rendez-vous par téléphone
-            </p>
-            <Link className="subHeader-link" to="">Infos contact</Link>
-          </div>
-        </div>
-        <div className="subHeader-container-col">
-          <div className="subHeader-container-heading">
-            <img src={RDV} alt="rendez-vous" />
-            <h3 className="subHeader-title">Horaires</h3>
-          </div>
-          <div className="subHeader-container-content">
-            <p>
-              Vous pouvez prendre 
-              séances se déroulent 
-            </p>
-          </div>
-        </div>
+      ))}
       </div>
     </section>
   );
---
+++ src/data/subheader.texts.js
@@ -0,0 +1,33 @@
+import RDV from "../assets/subheader/rdv.svg";
+import LOCATION  from "../assets/subheader/clock.svg";
+import CLOCK from "../assets/subheader/clock.svg";
+import PHONE from "../assets/subheader/phone.svg";
+
+const data = [
+  {
+    icon: RDV,
+    title: "Rendez-vous",
+    content: "Vous pouvez prendre rendez-vous par téléphone ou email.",
+    link: "Infos contact",
+  },
+  {
+    icon: LOCATION,
+    title: "Adresse",
+    content: "Vous pouvez prendre rendez-vous par téléphone ou email.",
+    link: "Voir sur la carte",
+  },
+  {
+    icon: PHONE,
+    title: "Contact",
+    content: "Vous pouvez prendre rVous pouvez prendre rendez-vous par téléphone Vous",
+    link: "Infos contact",
+  },
+  {
+    icon: CLOCK,
+    title: "Horaires",
+    content: "Vous pouvez prendre rVous pouvez prendre rendez-vous par téléphone Vous",
+  }, 
+];
+
+export default data;
+
